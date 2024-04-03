0b8502867951f311555eb0b626456293737f56c2
dmzz-yyhyy <hk198580666@outlook.com>
2024-04-03 22:27:46 +0800

    #3 将ip解析放入线程

--- src/main/kotlin/indi/nightfish/potato_ip_display/listener/PlayerJoinListener.kt
+++ src/main/kotlin/indi/nightfish/potato_ip_display/listener/PlayerJoinListener.kt
@@ -1,13 +1,14 @@
 package indi.nightfish.potato_ip_display.listener
 
-import indi.nightfish.potato_ip_display.util.IpAttributeMap
 import indi.nightfish.potato_ip_display.PotatoIpDisplay
 import indi.nightfish.potato_ip_display.parser.IpParseFactory
+import indi.nightfish.potato_ip_display.util.IpAttributeMap
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerLoginEvent
+import org.bukkit.scheduler.BukkitRunnable
 
 
 class PlayerJoinListener : Listener {
@@ -16,20 +17,25 @@ class PlayerJoinListener : Listener {
 
     @EventHandler
     fun onPlayerLogin(event: PlayerLoginEvent) {
-        val playerAddress = event.realAddress.hostAddress
-        val playerName = event.player.name
-        val ipParse = IpParseFactory.getIpParse(playerAddress)
-        var result = ipParse.getProvince()
 
-        if (result == "未知" || result == "") {
-            result = ipParse.getCity()
-            if (result == "未知" || result == "") {
-                result = ipParse.getCountry()
-            }
-        }
+        object : BukkitRunnable() {
+                override fun run() {
+                    val playerAddress = event.realAddress.hostAddress
+                    val playerName = event.player.name
+                    val ipParse = IpParseFactory.getIpParse(playerAddress)
+                    var result = ipParse.getProvince()
 
-        IpAttributeMap.playerIpAttributeMap[playerName] = result
-        plugin.log("Player named $playerName connect to proxy from ${ipParse.getProvince()}${ipParse.getCity()} ${ipParse.getISP()}")
+                    if (result == "未知" || result == "") {
+                        result = ipParse.getCity()
+                        if (result == "未知" || result == "") {
+                            result = ipParse.getCountry()
+                        }
+                    }
+
+                    IpAttributeMap.playerIpAttributeMap[playerName] = result
+                    plugin.log("Player named $playerName connect to proxy from ${ipParse.getProvince()}${ipParse.getCity()} ${ipParse.getISP()}")
+                }
+            }.run()
     }
 
     @EventHandler
