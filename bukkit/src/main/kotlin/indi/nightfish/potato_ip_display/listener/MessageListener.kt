14b2515f0a8d5577f51700119d42c53c87f5c7b5
yukonisen <whenten99@gmail.com>
2024-04-22 20:55:52 +0800

    improve MessageListener

--- bukkit/src/main/kotlin/indi/nightfish/potato_ip_display/listener/MessageListener.kt
+++ bukkit/src/main/kotlin/indi/nightfish/potato_ip_display/listener/MessageListener.kt
@@ -1,9 +1,10 @@
 package indi.nightfish.potato_ip_display.listener
 
-import indi.nightfish.potato_ip_display.util.IpAttributeMap
 import indi.nightfish.potato_ip_display.PotatoIpDisplay
+import indi.nightfish.potato_ip_display.util.IpAttributeMap
 import me.clip.placeholderapi.PlaceholderAPI
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -14,18 +15,17 @@ class MessageListener : Listener {
     private val plugin = Bukkit.getPluginManager().getPlugin("PotatoIpDisplay") as PotatoIpDisplay
     private val conf = plugin.conf
     @EventHandler(priority = EventPriority.LOWEST)
+
     fun onPlayerChat(event: AsyncPlayerChatEvent) {
         val playerName = event.player.name
-        var msg = event.message
         val ipAttr = IpAttributeMap.playerIpAttributeMap[playerName] ?: "未知"
 
-        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            msg = PlaceholderAPI.setPlaceholders(event.player, msg)
-        }
-        Bukkit.getServer().broadcastMessage(conf.message.playerChat.string
+        val chatmsg = (conf.message.playerChat.string
             .replace("%ipAttr%", ipAttr)
-            .replace("%playerName%", playerName)
-            .replace("%msg%", msg))
-        event.isCancelled = true
+            .replace("%playerName%", "%1\$s")
+            .replace("%msg%", "%2\$s"))
+
+        event.format = chatmsg
     }
+
 }
