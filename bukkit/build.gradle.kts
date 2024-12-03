c9720410f5028b314a4ef9c2a32577574b5366eb
QiuSYan <halo-402@outlook.com>
2024-12-03 16:40:20 +0800

    Update build.gradle.kts

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 repositories {
     mavenCentral()
     maven ("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    maven ("https://repo.helpch.at/me")
+    maven ("https://repo.helpch.at/releases")
 }
 
 dependencies {
